cbc2db5c4e77b6a923733aacab09a67ad25c41e3
xniebuhr <xniebuhr@gmail.com>
2024-10-10 20:18:13 -0500

    Create Menus.h

---
+++ Header files/Menus.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Books.h"
+#include "Employees.h"
+#include "Members.h"
+#include "Purchases.h"
+#include "Sales.h"
+
+void mainMenu();
+
+void bookMenu();
+
+void purchaseMenu();
+
+void employeeMenu();
+
+void memberMenu();
+
+void salesMenu();
